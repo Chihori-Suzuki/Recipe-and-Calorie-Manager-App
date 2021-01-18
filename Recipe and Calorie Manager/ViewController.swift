767d4abc2f80df8e04d8ea0ead7613cb9e422b9c
Macbook Pro <gmjetomo@outlook.com>
2021-01-18 14:29:53 -0800

    first commit

---
+++ Recipe and Calorie Manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Recipe and Calorie Manager
+//
+//  Created by Macbook Pro on 2021-01-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
