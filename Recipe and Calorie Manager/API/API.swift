81357f64d60fb1d2d99c7dce68738ccb690d8931
Macbook Pro <gmjetomo@outlook.com>
2021-01-18 16:35:27 -0800

    added API.swift

---
+++ Recipe and Calorie Manager/API/API.swift
@@ -0,0 +1,12 @@
+//
+//  API.swift
+//  Recipe and Calorie Manager
+//
+//  Created by Macbook Pro on 2021-01-18.
+//
+
+import Foundation
+
+struct API {
+    static let key = "MoFeiGHD8or5KOC20DT2JQ==n6AAVoxLVYuHO9xe"
+}
