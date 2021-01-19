da69d603c4a8b97698adf78184f6f70b5f45f3d3
Chihori Suzuki <moonwetwithhoney@mail.com>
2021-01-18 16:04:10 -0800

    commit

---
+++ Recipe and Calorie Manager/API/API.swift
@@ -0,0 +1,12 @@
+//
+//  API.swift
+//  Recipe and Calorie Manager
+//
+//  Created by 鈴木ちほり on 2021/01/18.
+//
+
+import Foundation
+
+struct API {
+    let key = "cCJANiskDtv5WSEfG1Z9TQ==eLfEisrOE3PhuyOr"
+}
