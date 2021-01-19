9895607d463948e84c4ed8403a915682651726b7
Macbook Pro <gmjetomo@outlook.com>
2021-01-18 17:48:35 -0800

    added API fetch functionality

--- Recipe and Calorie Manager/API/Nutrition.swift
+++ Recipe and Calorie Manager/API/Nutrition.swift
@@ -7,6 +7,38 @@
 
 import Foundation
 
-struct Nutrition {
-    //coment
+struct Ingredient: Codable {
+    var items: [Nutrition]
 }
+
+struct Nutrition: Codable {
+    var sugar: Double
+    var fiber: Double
+    var serving: Double
+    var sodium: Double
+    var name: String
+    var potassium: Double
+    var fat: Double
+    var totalFat: Double
+    var calories: Double
+    var cholesterol: Double
+    var protein: Double
+    var carbohydrates: Double
+    
+    enum CodingKeys: String, CodingKey  {
+        case sugar = "sugar_g"
+        case fiber = "fiber_g"
+        case serving = "serving_size_g"
+        case sodium = "sodium_mg"
+        case name = "name"
+        case potassium = "potassium_mg"
+        case fat = "fat_saturated_g"
+        case totalFat = "fat_total_g"
+        case calories = "calories"
+        case cholesterol = "cholesterol_mg"
+        case protein = "protein_g"
+        case carbohydrates = "carbohydrates_total_g"
+    }
+}
+
+
--- Recipe and Calorie Manager/ViewController.swift
+++ Recipe and Calorie Manager/ViewController.swift
@@ -11,7 +11,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let query = "3lb carrots and a chicken sandwich".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        let url = URL(string: "https://api.calorieninjas.com/v1/nutrition?query="+query!)!
+        var request = URLRequest(url: url)
+        request.setValue(API.key, forHTTPHeaderField: "X-Api-Key")
+        let task = URLSession.shared.dataTask(with: request) {(data, response, error) in
+            guard let data = data else { return }
+
+            let decoder = JSONDecoder()
+              do {
+                let ingredient = try decoder.decode(Ingredient.self, from: data)
+                print(ingredient.items)
+//                for nutrition in ingredient.items {
+//                    print(nutrition.calories)
+//                }
+              } catch {
+                print(error)
+              }
+        }
+        task.resume()
     }
 
 
