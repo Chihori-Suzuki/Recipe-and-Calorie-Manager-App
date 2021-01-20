8513797828135bcc9f60be3112546721ca49395f
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2021-01-19 16:26:37 -0800

    Make tableViewCells

--- Recipe and Calorie Manager/ViewControllers/RecipeListViewController.swift
+++ Recipe and Calorie Manager/ViewControllers/RecipeListViewController.swift
@@ -8,25 +8,48 @@
 import UIKit
 
 
-class RecipeListViewController: UIViewController {
-
+class RecipeListViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+    
+    let cellId = "RecipeListCell"
+    let meals = ["Breakfast","Lunch","Snacks","Dinner"]
+    
+    let myTable: UITableView = {
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        return table
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green
         title = "Recipe List"
         navigationController?.navigationBar.prefersLargeTitles = true
-        // Do any additional setup after loading the view.
+        view.addSubview(myTable)
+        setupTableView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupTableView() {
+        myTable.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        myTable.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        myTable.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        myTable.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        myTable.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        myTable.delegate = self
+        myTable.dataSource = self
     }
-    */
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return meals.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        cell.textLabel!.text = meals[indexPath.row]
+        return cell
+    }
 }
