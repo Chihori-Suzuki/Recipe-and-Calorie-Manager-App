d1daee23ff1e3baeaacba366b238b0dc85d09694
Macbook Pro <gmjetomo@outlook.com>
2021-01-21 13:00:04 -0800

    added button images and labels

--- Recipe and Calorie Manager/ViewControllers/AddRecipeViewController.swift
+++ Recipe and Calorie Manager/ViewControllers/AddRecipeViewController.swift
@@ -21,37 +21,118 @@ class AddRecipeViewController: UIViewController {
         return tf
     }()
     
-    let addButton: UIButton = {
-       let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Add New Recipe", for: .normal)
-        button.backgroundColor = .systemBlue
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 25)
-        button.widthAnchor.constraint(equalToConstant: 250).isActive = true
-        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        button.layer.cornerRadius = 8
-        button.addTarget(self, action: #selector(addNewRecipe), for: .touchUpInside)
-        button.isEnabled = false
-        button.alpha = 0.5
-    return button
-    }()
+    private var meals = ["breakfast", "lunch", "dinner", "snacks"]
+    private var breakfastButton = UIButton()
+    private var snacksButton = UIButton()
+    private var lunchButton = UIButton()
+    private var dinnerButton = UIButton()
+    
+    private var mealButtons: [UIButton] {
+        let arr = [breakfastButton, lunchButton, dinnerButton, snacksButton]
+        return arr
+    }
     
     let vStackView: UIStackView = {
         let sv = UIStackView()
         sv.translatesAutoresizingMaskIntoConstraints = false
         sv.axis = .vertical
-        sv.alignment = .fill
-        sv.spacing = 8
+        sv.alignment = .center
+        sv.distribution = .fill
+        sv.spacing = 40
         return sv
     }()
     
+    fileprivate func makeButtons(with image: String) -> UIButton {
+        let btn = UIButton()
+        btn.setImage(UIImage(named: image), for: .normal)
+        btn.imageView?.contentMode = .scaleAspectFit
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.isEnabled = false
+        btn.alpha = 0.5
+        btn.addTarget(self, action: #selector(mealSelected(_:)), for: .touchUpInside)
+        return btn
+    }
+    
+    func makeMealLabel(with string: String) -> UILabel {
+        let label = UILabel()
+        label.text = string
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        label.font = UIFont.boldSystemFont(ofSize: 25)
+        label.layer.cornerRadius = 6
+        return label
+    }
+    
+    fileprivate func arrangeHStackViews(with buttons: [UIButton], and labels: [UILabel]) -> UIStackView {
+       
+        let hSVButtonLabels1 = UIStackView()
+        let hSVButtonLabels2 = UIStackView()
+        for (index, _) in buttons.enumerated(){
+            let sv = UIStackView(arrangedSubviews: [buttons[index], labels[index]])
+            sv.axis = .vertical
+            sv.distribution = .fill
+            sv.widthAnchor.constraint(equalToConstant: 110).isActive = true
+            sv.alignment = .center
+            sv.spacing = 8
+            sv.translatesAutoresizingMaskIntoConstraints = false
+            index > 1 ? hSVButtonLabels2.addArrangedSubview(sv) : hSVButtonLabels1.addArrangedSubview(sv)
+        }
+        
+        hSVButtonLabels1.axis = .horizontal
+        hSVButtonLabels1.alignment = .center
+        hSVButtonLabels1.distribution = .fill
+        hSVButtonLabels1.translatesAutoresizingMaskIntoConstraints = false
+        hSVButtonLabels1.spacing = 30
+        
+        hSVButtonLabels2.axis = .horizontal
+        hSVButtonLabels2.alignment = .center
+        hSVButtonLabels2.distribution = .fill
+        hSVButtonLabels2.translatesAutoresizingMaskIntoConstraints = false
+        hSVButtonLabels2.spacing = 30
+        
+        let sv = UIStackView(arrangedSubviews: [hSVButtonLabels1, hSVButtonLabels2])
+        sv.axis = .vertical
+        sv.distribution = .fill
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        sv.alignment = .center
+        sv.spacing = 25
+        return sv
+    }
+    
+    @objc func mealSelected(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.10) {
+            sender.transform = CGAffineTransform(scaleX: 0.6, y: 0.6)
+        } completion: { (_) in
+            UIView.animate(withDuration: 0.10) {
+                sender.transform = CGAffineTransform.identity
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Add New Recipe"
         navigationController?.navigationBar.prefersLargeTitles = true
         view.backgroundColor = .white
         
+        breakfastButton = makeButtons(with: "breakfast")
+        lunchButton = makeButtons(with: "lunch")
+        snacksButton = makeButtons(with: "snack")
+        dinnerButton = makeButtons(with: "dinner")
+        
+        var mealLabels = [UILabel]()
+        for meal in meals {
+            let label = makeMealLabel(with: meal)
+            mealLabels.append(label)
+        }
+        
+        let vMealStackView = arrangeHStackViews(with: [breakfastButton, lunchButton, dinnerButton, snacksButton], and: mealLabels)
+        
         vStackView.addArrangedSubview(recipeTextField)
-        vStackView.addArrangedSubview(addButton)
+        vStackView.addArrangedSubview(vMealStackView)
+        
         view.addSubview(vStackView)
         
         vStackView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor).isActive = true
@@ -66,12 +147,15 @@ class AddRecipeViewController: UIViewController {
     
     @objc func textEditingChanged(_ sender: UITextField) {
         guard let text = sender.text, text.count > 3 else {
-            addButton.alpha = 0.5
-            addButton.isEnabled = false
+            for button in mealButtons {
+                button.alpha = 0.5
+                button.isEnabled = false
+            }
             return
         }
-        addButton.alpha = 1.0
-        addButton.isEnabled = true
+        for button in mealButtons {
+            button.alpha = 1.0
+            button.isEnabled = true
+        }
     }
-
 }
