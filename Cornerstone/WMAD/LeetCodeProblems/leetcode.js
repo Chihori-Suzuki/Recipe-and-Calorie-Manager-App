4b608a8c1ae45531ea6ae1f327dba160db8b2940
Chihori Suzuki <moonwetwithhoney@mail.com>
2021-06-26 13:52:47 -0700

    commit

--- Cornerstone/WMAD/LeetCodeProblems/leetcode.js
+++ Cornerstone/WMAD/LeetCodeProblems/leetcode.js
@@ -101,20 +101,15 @@ var sortedSquares = function (nums) {
  * }
  */
 
-function TreeNode(val, left, right) {
-  this.val = val === undefined ? 0 : val;
-  this.left = left === undefined ? null : left;
-  this.right = right === undefined ? null : right;
-}
 /**
  * @param {TreeNode} root
  * @return {number}
  */
 var maxDepth = function (root) {
-  dfs(root, 0);
   function dfs(root, height) {
-    if (root === null) return 0;
-
+    if (root == null) return height;
     return Math.max(dfs(root.left, height + 1), dfs(root.right, height + 1));
   }
+
+  return dfs(root, 0);
 };
